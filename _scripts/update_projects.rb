a5ce5578001be376c950e5cb191370cf94147553
Klaus Badelt <klaus@kinonation.com>
2013-12-18 03:08:10 +0800

    light theme. grid now default. update-script expects music now in /mx folder, builds the site and uploads everything to S3. Requires s3cmd.

--- _scripts/update_projects.rb
+++ _scripts/update_projects.rb
@@ -3,24 +3,30 @@ require 'taglib'
 require 'base64'
 require 'uri'
 
-POOL = "https://s3.amazonaws.com/pool.klausbadelt"
+SITE_URL = 'https://s3.amazonaws.com/klausbadelt-com'
+MX_FOLDER = 'mx'
 
-puts "This script will write all mp3 albums in ../mx as new posts."
-puts "Set ID3 tags for album, title, release and comment (as Buy URL)"
-print "Continue (y/n)? "
+print "This script will write all mp3 albums in ../#{MX_FOLDER} as new posts,
+generate the site html and sync it to S3.
+Put all mp3s in folders under ../#{MX_FOLDER} named after the project ID (3 letter code).
+Set ID3 tags for album, title, release and comment (as Buy URL).
+Each album needs a poster sized 300×430px named <project-ID>.jpg
+stored in the folder ../images/posters.
+
+Continue (y/n)? "
 exit 1 unless gets == "y\n"
 puts 'Scanning mp3s...'
-# read all projects as subfolders in mx/*
-Dir.glob('../mx/**') do |folder|
+# read all projects as subfolders in MX_FOLDER /*
+Dir.glob("../#{MX_FOLDER}/**") do |folder|
   project = File.basename(folder)
   
   # don't overwrite existing post - test for match with anyt date (2013-10-03-project.markup)
   # unless (match = Dir.glob(File.join('../_posts/*')).grep(/\d*-\d*-\d*-#{project}\./)).empty?
-  #   puts "#{project} already exists: #{File.basename(match.first)}. Skip."
+  #   puts "#{project} already exists: #{File.basename(match.first)}. Skipping."
   #   next
   # end
   
-  # get global vars from first mp3
+  # get 'global' vars from first mp3
   audiofiles = Dir.glob(File.join(folder,'*.mp3'))
   mp3 = TagLib::FileRef.new audiofiles.first
   album = mp3.tag.album.gsub(/ \[.*\]/, '').tr(':""','')
@@ -28,10 +34,9 @@ Dir.glob('../mx/**') do |folder|
   buy = mp3.tag.comment if mp3.tag.comment =~ /^#{URI::ABS_URI}$/ # check for valid URL
   mp3.close
 
-    
   # write post front matter
   postname = "../_posts/#{release.strftime("%Y-%m-%d")}-#{project}.markdown"
-  puts "Writing \"#{album}\""
+  puts "Writing '#{album}'"
   File.open(postname, 'w') do |post|
     post.write(<<eos)
 ---
@@ -48,7 +53,7 @@ eos
     # write tracklisting
     audiofiles.each do |audiofile|
       TagLib::FileRef.open audiofile do |mp3|
-        href = Base64.encode64("#{POOL}/#{project}/#{File.basename(audiofile)}").tr("\n","").chomp
+        href = Base64.encode64("#{SITE_URL}/#{MX_FOLDER}/#{project}/#{File.basename(audiofile)}").tr("\n","").chomp
         title = mp3.tag.title.tr(':','')
         duration = Time.at(mp3.audio_properties.length).utc.strftime("%M:%S")
       
@@ -62,4 +67,6 @@ eos
 end
 
 puts 'Uploading site...'
+system 'cd .. && jekyll build && s3cmd sync --rr -P --delete-removed _site/ s3://www.klausbadelt.com'
+puts 'Uploading music...'
 system 'cd .. && jekyll build && s3cmd sync --rr -P --delete-removed _site/ s3://www.klausbadelt.com'
